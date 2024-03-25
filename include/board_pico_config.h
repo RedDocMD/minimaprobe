ee6df281bfca125970f4dc2c5972715a88912b19
Deep Majumder <deep.majumder2019@gmail.com>
2024-03-26 00:46:23 +0530

    Flipped values for io and clk pins

--- include/board_pico_config.h
+++ include/board_pico_config.h
@@ -32,8 +32,8 @@
 // PIO config
 #define PROBE_SM 0
 #define PROBE_PIN_OFFSET 2
-#define PROBE_PIN_SWCLK (PROBE_PIN_OFFSET + 0) // 2
-#define PROBE_PIN_SWDIO (PROBE_PIN_OFFSET + 1) // 3
+#define PROBE_PIN_SWCLK (PROBE_PIN_OFFSET + 1) // 3
+#define PROBE_PIN_SWDIO (PROBE_PIN_OFFSET + 0) // 2
 // Target reset config
 #if false
 #define PROBE_PIN_RESET 1
